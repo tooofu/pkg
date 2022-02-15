16d470e297982b554b8d99800575251571097bf1
Jung <1024.ding@gmail.com>
2022-02-15 12:07:29 +0800

    dev

---
+++ ioutils/buffer.go
@@ -0,0 +1,13 @@
+package ioutils
+
+import (
+	"bufio"
+)
+
+func Pop(br *bufio.Reader, n int) (bs []byte, err error) {
+	bs, err = br.Peek(n)
+	if err == nil {
+		_, err = br.Discard(n)
+	}
+	return
+}
